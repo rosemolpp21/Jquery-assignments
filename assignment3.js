e1231464ce82afb70862736263ef1922ee3799d0
rosemo21 <rosemolpp2001@gmail.com>
2024-12-11 10:19:35 +0530

    assignment3 submission

---
+++ assignment3.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $("#form-submit").on("click", function (event) {
+        event.preventDefault();
+        $(".error").html("");
+        $("#succes-msg").html("");
+        let valid = true;
+        let fullname = $("#fname").val().trim();
+        let emailid = $("#emailid").val().trim();
+        let feedback = $("#feedback").val().trim();
+        if (fullname.length < 3) {
+            $("#name-error").html("name should have atleast 3 letters");
+            valid = false;
+        }
+        const emailPattern = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+        if (!emailPattern.test(emailid)) {
+            $('#mail-error').text('Enter a valid email id');
+            valid = false;
+        }
+        if (feedback.length < 10) {
+            $("#feedback-error").html("feedback should conatain atleast 10 characters");
+            valid = false;
+        }
+        if (!valid) {
+            return;
+        }
+        $.post("https://jsonplaceholder.typicode.com/posts",
+            { Name: `${fullname}`, Email: `${emailid}`, Feedback: `${feedback}` },
+            function (data, status) {
+                console.log(data);
+                if (status == "success") {
+                    $("#succes-msg").append("Feedback submitted successfully")
+                    for (i in data) {
+                        $("#succes-msg").append(`<br>${i} : ${data[i]}`)
+                    }
+                    $('#myform')[0].reset();
+                }
+                else {
+                    $("#succes-msg").html("Data delivery is unsuccessfull");
+                }
+            });
+
+    })
+})
+
